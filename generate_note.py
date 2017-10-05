e17117d74f0ae8b9944cbd6790394cd3b3e67a9d
Abraham Polk <abepolk@gmail.com>
2017-10-05 15:06:59 -0400

    use argparse in generateNote

---
+++ generate_note.py
@@ -0,0 +1,35 @@
+from write_wav import writeWave
+from collections import deque
+import random
+import numpy as np
+import argparse
+
+
+# generate note of given frequency using Karplus algorithm
+def generateNote(freq):
+    nSamples = 44100
+    sampleRate = 44100
+    N = int(sampleRate/freq)
+    # initialize ring buffer
+    buf = deque([random.random() - 0.5 for i in range(N)])
+    # plot of flag set 
+    # init sample buffer
+    samples = np.array([0]*nSamples, 'float32')
+    for i in range(nSamples):
+        samples[i] = buf[0]
+        avg = 0.995*0.5*(buf[0] + buf[1])
+        buf.append(avg)
+        buf.popleft() 
+      
+    # samples to 16-bit to string
+    # max value is 32767 for 16-bit
+    samples = np.array(samples * 32767, 'int16')
+    return samples.tostring()
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Generating sounds with Karplus String Algorithm.")
+    # add arguments
+    parser.add_argument('--freq', default=440, type=int, required=False)
+    args = parser.parse_args()   
+    data = generateNote(int(args.freq)) # YOu need argparser to get x
+    writeWave('note.wav', data)
